f9adbd3a1b1aafa249537c81e732df75ecbb9387
Mae Chester-Jones <mae.chester-jones@ndorms.ox.ac.uk>
2024-08-30 12:36:15 +0100

    Updated with all runs

--- make_html.R
+++ make_html.R
@@ -1,5 +1,6 @@
 library("quarto")
-
+# WD 
+setwd("C:\\Users\\maecj\\OneDrive - Nexus365\\A DPhil\\Simulation studies\\Programs\\Study 1\\SimulationStudy1_11Jun2024\\QuartoResults")
 # Render the entire project to html
 quarto_render(input = ".",
               output_format = "html", as_job=FALSE)
