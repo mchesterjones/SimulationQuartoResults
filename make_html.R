fb6bf1621d1380759c9ab27cd5e2125ae7133b93
Mae Chester-Jones <mae.chester-jones@ndorms.ox.ac.uk>
2024-11-12 09:57:44 +0000

    html and latex output formats

--- make_html.R
+++ make_html.R
@@ -3,4 +3,9 @@ library("quarto")
 setwd("C:\\Users\\maecj\\OneDrive - Nexus365\\A DPhil\\Simulation studies\\Programs\\Study 1\\SimulationStudy1_11Jun2024\\QuartoResults")
 # Render the entire project to html
 quarto_render(input = ".",
-              output_format = "html", as_job=FALSE)
+              output_format = "html", 
+              as_job=FALSE)
+
+quarto_render(input = ".",
+             output_format = "latex", 
+             as_job = FALSE)
